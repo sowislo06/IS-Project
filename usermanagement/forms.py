dc8bd16965a8e89f16bcba139b4b87208d62e436
sowislo06 <julius.wessing95@gmail.com>
2020-05-04 15:11:14 +0200

    Add Usermangement. Register, Login, Logout

---
+++ usermanagement/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=35, label='Username eintragen')
+    password = forms.CharField(min_length=6, max_length=50, label='Passwort eintragen', widget=forms.PasswordInput)
+    email = forms.EmailField(label='Email eintragen')
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username__iexact=username).exists():
+            raise forms.ValidationError('Diesen Usernamen gibt es bereits')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError('Diese Email gibt es bereits')
+        return email
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length = 35, label='Username eintragen')
+    password = forms.CharField(min_length=6, max_length = 50, label='Passwort eintragen', widget=forms.PasswordInput)
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-#Method 'index'
+from .forms import RegisterForm, LoginForm
+
+
 def index(request):
     return render(request, 'usermanagement/base.html')
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('usermanagement:index'))
+        #return redirect(reverse('blog:timeline', kwargs={'username': request.user.username}))
+    register_form = RegisterForm()
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password = register_form.cleaned_data['password']
+            email = register_form.cleaned_data['email']
+            User.objects.create_user(username=username, password=password, email=email)
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect(reverse('usermanagement:index'))
+                #return redirect(reverse('blog:timeline', kwargs={'username': request.user.username}))
+    return render(request, 'usermanagement/register.html', {'register_form': register_form})
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('usermanagement:index'))
+        #return redirect(reverse('blog:timeline', kwargs={'username': request.user.username}))
+    login_form = LoginForm()
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect(reverse('usermanagement:index'))
+                #return redirect(reverse('blog:timeline', kwargs={'username': request.user.username}))
+    return render(request, 'usermanagement/login.html', {'login_form': login_form})
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('usermanagement:index'))
